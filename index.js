63d13cef7f7801e47d28537788e18423c31d5cc5
haiducvu <duchaivu1997@gmail.com>
2020-02-27 08:17:37 +0700

    27/2/2020-login-screen

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-import login from'./src/components/login';
+import login from'./src/containers/login';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => login);
